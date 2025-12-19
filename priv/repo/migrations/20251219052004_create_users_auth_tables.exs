701a2374ad08c9df8564f933911916c4d81a58e2
Dean Kinyua <kinyuadean@gmail.com>
2025-12-19 14:33:43 +0300

    create the migration file for authentication

---
+++ priv/repo/migrations/20251219052004_create_users_auth_tables.exs
@@ -0,0 +1,30 @@
+defmodule VideoCall.Repo.Migrations.CreateUsersAuthTables do
+  use Ecto.Migration
+
+  def change do
+    execute "CREATE EXTENSION IF NOT EXISTS citext", ""
+
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :email, :citext, null: false
+      add :hashed_password, :string, null: false
+      add :username, :string, null: false
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:email, :username])
+
+    create table(:users_tokens, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :context, :string, null: false
+      add :token, :binary, null: false
+      add :user_id, references(:users, type: :binary_id, on_delete: :delete_all)
+
+      timestamps(updated_at: false)
+    end
+
+    create index(:users_tokens, [:user_id])
+    create unique_index(:users_tokens, [:context, :token])
+  end
+end
