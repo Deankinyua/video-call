10bd6e542617011a155b0ab39a53b88a7fffe2b2
Dean Kinyua <kinyuadean@gmail.com>
2025-12-19 17:44:31 +0300

    add eye icon

--- lib/video_call_web/components/core_components.ex
+++ lib/video_call_web/components/core_components.ex
@@ -366,7 +366,41 @@ defmodule VideoCallWeb.CoreComponents do
     """
   end
 
-  # All other inputs text, datetime-local, url, password, etc. are handled here...
+  def input(%{type: "password"} = assigns) do
+    ~H"""
+    <div>
+      <.label for={@id}>{@label}</.label>
+      <div class="relative">
+        <input
+          type={@type}
+          name={@name}
+          id={@id}
+          placeholder="***********"
+          value={Phoenix.HTML.Form.normalize_value(@type, @value)}
+          class={[
+            "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
+            @errors == [] && "border-zinc-300 focus:border-zinc-400",
+            @errors != [] && "border-rose-400 focus:border-rose-400"
+          ]}
+          {@rest}
+        />
+        <.eye_icon
+          id={"eye-icon-#{@id}"}
+          input_id={@id}
+          class="w-5 absolute top-3 right-3 cursor-pointer"
+        />
+        <.eye_off_icon
+          id={"eye-off-icon-#{@id}"}
+          input_id={@id}
+          class="w-5 absolute top-3 right-3 hidden cursor-pointer"
+        />
+      </div>
+      <.error :for={msg <- @errors}>{msg}</.error>
+    </div>
+    """
+  end
+
+  # All other inputs text, datetime-local, url, etc. are handled here...
   def input(assigns) do
     ~H"""
     <div>
@@ -566,6 +600,79 @@ defmodule VideoCallWeb.CoreComponents do
         <.icon name="hero-arrow-left-solid" class="h-3 w-3" />
         {render_slot(@inner_block)}
       </.link>
+    </div>
+    """
+  end
+
+  @doc """
+  Renders a eye_off icon
+  """
+
+  attr :class, :string, default: ""
+  attr :id, :any, required: true
+  attr :input_id, :any, required: true
+
+  def eye_off_icon(assigns) do
+    ~H"""
+    <div
+      id={@id}
+      class={@class}
+      phx-click={
+        JS.set_attribute({"type", "password"}, to: "##{@input_id}")
+        |> JS.toggle(to: "##{@id}")
+        |> JS.toggle(to: "#eye-icon-#{@input_id}")
+      }
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+        />
+      </svg>
+    </div>
+    """
+  end
+
+  @doc """
+  Renders a eye icon
+  """
+
+  attr :class, :string, default: ""
+  attr :id, :any, required: true
+  attr :input_id, :any, required: true
+
+  def eye_icon(assigns) do
+    ~H"""
+    <div
+      id={@id}
+      class={@class}
+      phx-click={
+        JS.set_attribute({"type", "text"}, to: "##{@input_id}")
+        |> JS.toggle(to: "##{@id}")
+        |> JS.toggle(to: "#eye-off-icon-#{@input_id}")
+      }
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+        />
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+      </svg>
     </div>
     """
   end
