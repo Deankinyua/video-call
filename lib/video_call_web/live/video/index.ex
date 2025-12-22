b2de6e38f01df45d1518bafa8ab91a2d291d2fd0
Dean Kinyua <kinyuadean@gmail.com>
2025-12-22 18:21:27 +0300

    Dummy contacts

--- lib/video_call_web/live/video/index.ex
+++ lib/video_call_web/live/video/index.ex
@@ -1,12 +1,28 @@
 defmodule VideoCallWeb.VideoLive.Index do
   use VideoCallWeb, :live_view
 
+  import VideoCallWeb.ContactComponents
+
+  alias VideoCall.Accounts
+
   @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
-    <div class="flex flex-col items-center">
-      Welcome to our Video
+    <div class="flex flex-col items-center relative">
+      <div id="contacts" class="w-[24rem] absolute top-8 right-8">
+        <div class="mb-2">Contacts</div>
+        <div :for={contact <- @contacts}>
+          <.contact_component username={contact.username} />
+        </div>
+      </div>
     </div>
     """
+  end
+
+  @impl Phoenix.LiveView
+  def mount(_params, _session, socket) do
+    contacts = Accounts.list_users()
+
+    {:ok, assign(socket, :contacts, contacts)}
   end
 end
