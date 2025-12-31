32a2438707dbe1a13d7cb40f05f10bf2245bc514
Dean Kinyua <kinyuadean@gmail.com>
2025-12-31 09:59:00 +0300

    remove comments

--- assets/js/hooks/rtc_connection.js
+++ assets/js/hooks/rtc_connection.js
@@ -6,9 +6,9 @@ RtcConnectionHooks.RtcConnection = {
     this.localVideoEl = document.querySelector("#local-video");
     this.remoteVideoEl = document.querySelector("#remote-video");
 
-    this.localStream; //a var to hold the local video stream
-    this.remoteStream; //a var to hold the remote video stream
-    this.peerConnection; //the peerConnection that the two clients use to talk
+    this.localStream;
+    this.remoteStream;
+    this.peerConnection;
     this.didIOffer = false;
 
     // * The STUN servers
@@ -53,14 +53,10 @@ RtcConnectionHooks.RtcConnection = {
   },
 
   async createOffer() {
-    //peerConnection is all set with our STUN servers sent over
     await this.createPeerConnection();
 
-    //create offer time!
     try {
-      console.log("Creating offer...");
       const offer = await this.peerConnection.createOffer();
-      console.log(offer.type); // Should be 'offer'
       // setLocalDescription is called so that the 2 peers eventually agree on a configuration
       this.peerConnection.setLocalDescription(offer);
       this.didIOffer = true;
@@ -86,7 +82,6 @@ RtcConnectionHooks.RtcConnection = {
           },
         });
 
-        // * so that you can see yourself on the screen
         this.localVideoEl.srcObject = stream;
         this.localStream = stream;
         resolve();
@@ -98,20 +93,12 @@ RtcConnectionHooks.RtcConnection = {
   },
 
   async answerOffer(offerObj) {
-    // On the receiver side we will do this using the offer
     await this.fetchUserMedia();
-    // * This will set the remoteDescription of CLIENT2 as the offer
     await this.createPeerConnection(offerObj);
     const answer = await this.peerConnection.createAnswer();
-    //this is CLIENT2, and CLIENT2 uses the answer as the localDesc
+
+    //* set answer as the localDescription of the answerer
     await this.peerConnection.setLocalDescription(answer);
-
-    //add the answer to the offerObj
-    // offerObj.answer = answer;
-
-    //emit the answer to the signaling server, so it can emit to CLIENT1
-    //expect a response from the server with the already existing offerer ICE candidates
-    // please note that this socket belongs to the answerer
 
     this.pushEvent("add_offerer_ice_candidates_to_answerer", {
       offerer: offerObj.offerer,
@@ -124,20 +111,17 @@ RtcConnectionHooks.RtcConnection = {
   },
 
   async addAnswer(answer) {
-    //addAnswer is called in socketListeners when an answerResponse is emitted.
-    //at this point, the offer and answer have been exchanged!
-    //now CLIENT1 needs to set the remote
+    //* set answer as remote description of the offerer
     await this.peerConnection.setRemoteDescription(answer);
     // and that's about it!!
   },
 
   createPeerConnection(offerObj) {
     return new Promise(async (resolve, reject) => {
-      //RTCPeerConnection is the thing that creates the connection
-      //we can pass a config object, and that config object can contain STUN/ICE servers
-      //which will fetch us ICE candidates
+      //note: RTCPeerConnection is the thing that creates the connection
+      //note: We need to pass the iceCandidates while initializing the peerConnection
+
       let pc = new RTCPeerConnection(this.peerConfiguration);
-      // set the remote stream to a new empty mediaStream
       this.remoteStream = new MediaStream();
       this.remoteVideoEl.srcObject = this.remoteStream;
 
@@ -148,17 +132,13 @@ RtcConnectionHooks.RtcConnection = {
       });
 
       pc.addEventListener("signalingstatechange", (event) => {
+        console.log("The signalling state has changed to...");
         console.log(pc.signalingState);
       });
 
-      // to check if icecandidates were generated
+      // check if icecandidates were generated
       pc.addEventListener("icecandidate", (e) => {
-        console.log("........Ice candidate found!......");
-        console.log(e.candidate);
         if (e.candidate) {
-          // This is how we are going propagate ice candidates to the other peer
-          // if didIOffer is true, then this ice candidates belong to the offerer, else they belong to the answerer
-
           this.pushEvent("send_ice_candidates_to_signalling_server", {
             did_i_offer: this.didIOffer,
             ice_candidate: e.candidate,
@@ -166,11 +146,11 @@ RtcConnectionHooks.RtcConnection = {
         }
       });
 
-      // This will be called when tracks added from the remote side
+      // This will be called when tracks are added from the remote side
       pc.addEventListener("track", (e) => {
-        console.log("Got a track from the other peer!! How excting");
-        console.log(e.streams);
+        console.log("I guess this is how we get the tracks");
         e.streams[0].getTracks().forEach((track) => {
+          console.log(track);
           this.remoteStream.addTrack(track);
         });
       });
@@ -178,10 +158,10 @@ RtcConnectionHooks.RtcConnection = {
       this.peerConnection = pc;
 
       if (offerObj) {
-        //this won't be set when called from call();
-        //will be set when we call from answerOffer()
+        // * This will set the remoteDescription of CLIENT2 as the offer
+        console.log("Setting remote description of the answerer...");
         await this.peerConnection.setRemoteDescription(offerObj.offer);
-        // console.log(peerConnection.signalingState) //should be have-remote-offer, because client2 has setRemoteDesc on the offer
+        console.log(peerConnection.signalingState); //should be have-remote-offer, because client2 has setRemoteDesc on the offer
       }
       resolve();
     });
