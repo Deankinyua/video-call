f6b67a4e489d63a422950678961cae634fa5f100
Dean Kinyua <kinyuadean@gmail.com>
2026-07-03 15:07:51 +0300

    about multimodal indices

--- priv/repo/migrations/20260112132131_contacts.exs
+++ priv/repo/migrations/20260112132131_contacts.exs
@@ -13,6 +13,16 @@ defmodule VideoCall.Repo.Migrations.Contacts do
       timestamps()
     end
 
-    create unique_index(:contacts, [:contact_user_id, :user_id])
+    create unique_index(:contacts, [:user_id, :contact_user_id])
+
+    # since we've created a multicolumn index above with the user_id being the
+    # leftmost column we don't need to do:
+    # * create index(:contacts, [:user_id])
+    # PG will use the above multicolumn index to speed up WHERE clauses involving :user_id
+    # such as queries that will be used to retrieve all contacts belonging to
+    # a specific user
+
+    # So in short look at what kind of queries you would be making and mutate
+    # the order of the columns in your multicolumn index
   end
 end
