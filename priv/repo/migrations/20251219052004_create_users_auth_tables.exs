60cefdbb3ff73d0220454dca17f84c1968ca5cc4
Dean Kinyua <kinyuadean@gmail.com>
2026-07-03 12:36:12 +0300

    comments

--- priv/repo/migrations/20251219052004_create_users_auth_tables.exs
+++ priv/repo/migrations/20251219052004_create_users_auth_tables.exs
@@ -13,7 +13,10 @@ defmodule VideoCall.Repo.Migrations.CreateUsersAuthTables do
       timestamps()
     end
 
+    # users cannnot share an email
     create unique_index(:users, [:email])
+
+    # users cannnot share a username
     create unique_index(:users, [:username])
 
     create table(:users_tokens, primary_key: false) do
@@ -25,7 +28,11 @@ defmodule VideoCall.Repo.Migrations.CreateUsersAuthTables do
       timestamps(updated_at: false)
     end
 
+    # this will make it much faster to search for tokens by using the user_id
+    # see Accounts.clear_all_tokens_for_user/1
     create index(:users_tokens, [:user_id])
+
+    # users cannot share the same combination of the context and the token
     create unique_index(:users_tokens, [:context, :token])
   end
 end
